d8835f2b19e4f55f0e2e6cfaab477ff8e303086f
pattymalone <114537268+pattymalone@users.noreply.github.com>
2025-06-02 17:16:26 -0400

    Update and rename index.tsx to mistenbyme

--- index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Welcome to Bradbury</h1>
-      <p>Your daily dose of curated poetry, news, music, and art.</p>
-    </main>
-  );
-}
