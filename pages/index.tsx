efd61c18107b4b62f586b139252f2eff5194eb6e
pattymalone <114537268+pattymalone@users.noreply.github.com>
2025-06-02 13:50:38 -0400

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to Bradbury</h1>
+      <p>Your daily dose of curated poetry, news, music, and art.</p>
+    </main>
+  );
+}
